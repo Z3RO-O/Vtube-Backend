6fd0b04460191e4d6e31ab3d9a93a388d8e676af
Z3RO-O <vaibhavsingh_21523@aitpune.edu.in>
2024-05-18 18:19:06 +0530

    adding utils for error handling and api response

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// for parsing application/json and limit is set to prevent DOS attack
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+
+// for parsing application/x-www-form-urlencoded or URL-encoded data which converts space and other special chars
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+// for storing files to keep them accessible to anyone
+app.use(express.static("public"));
+
+// for CRUD operations in cookies of users securely
+app.use(cookieParser());
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -4,8 +4,22 @@ import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.error("App Error: ", error);
+      throw error;
+    });
 
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection failed!!! : ", error);
+  });
+
+// iffie function which is called immediately
 /*
 import express from "express";
 const app = express();
